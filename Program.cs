995c975ab995815f13e115e8c8f58e0b8eaef3ce
RyanGregoire2024 <Ryangregoire286@gmail.com>
2024-11-08 09:09:36 -0500

    Added First Commit

--- Program.cs
+++ Program.cs
@@ -2,13 +2,32 @@
 
 namespace App {
 
-    class App {
+    public class Birthday {
 
-        static void Main(string[] args) 
+        public static void Main(string[] args) 
         {
-                        
-            Birthday.SetBirthday();
 
+            Console.WriteLine("Enter Birthday Month: ");
+            int birthdayMonth = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter Current Month: ");
+            int currentMonth = Convert.ToInt16(Console.ReadLine());
+
+
+            Console.WriteLine("Enter Your Birth Date: ");
+            int birthdayDaye = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter Current Day: ");
+            int currentDay = Convert.ToInt16(Console.ReadLine());
+
+            int dates = birthdayMonth - currentMonth;
+            int days = birthdayDaye - currentDay;
+
+            Console.WriteLine($"Your Birthday is in: {Math.Abs(dates)} Month");
+
+
+            Console.WriteLine($"Your Birthday is in: {Math.Abs(days)} Days");
+            
             Console.ReadLine();
 
         } 
